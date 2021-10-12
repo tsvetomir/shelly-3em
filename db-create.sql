77b38c00f9d7f9fb0c8ae0a2ebe563b244f877a2
Tsvetomir Tsonev <cvetomirconev@gmail.com>
2021-10-12 22:53:50 +0300

    feat: add sqlite backend

---
+++ db-create.sql
@@ -0,0 +1,12 @@
+CREATE TABLE stats(
+   date TEXT PRIMARY KEY,
+   voltage_1 REAL,
+   power_1 REAL,
+   power_usage_1 REAL,
+   voltage_2 REAL,
+   power_2 REAL,
+   power_usage_2 REAL,
+   voltage_3 REAL,
+   power_3 REAL,
+   power_usage_3 REAL
+);
